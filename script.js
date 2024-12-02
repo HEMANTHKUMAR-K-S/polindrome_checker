a6eea5837b72c3bfdf79817f3c9d863d8de753de
HEMANTHKUMAR K S <160527273+HEMANTHKUMAR-K-S@users.noreply.github.com>
2024-12-02 23:15:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const input =document.getElementById('input');
+ function reverseString(str){
+     return str.split('').reverse().join('');
+ }
+  function check(){
+    const value = input.value;
+    const reverse = reverseString(value);
+      if(value === reverse){
+          alert('This is a palindrome');
+      }else{
+          alert('This is not a palindrome');
+      }
+  }
